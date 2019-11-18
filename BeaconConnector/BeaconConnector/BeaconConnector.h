1ac78ea6631a53bf013833b1325b4cd7388df3de
Sandun <sandungeemal1996@gmail.com>
2019-11-18 13:02:49 +0800

    Finish BeaconConnector

---
+++ BeaconConnector/BeaconConnector/BeaconConnector.h
@@ -0,0 +1,19 @@
+//
+//  BeaconConnector.h
+//  BeaconConnector
+//
+//  Created by stone appy on 18/11/19.
+//  Copyright © 2019 stone appy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BeaconConnector.
+FOUNDATION_EXPORT double BeaconConnectorVersionNumber;
+
+//! Project version string for BeaconConnector.
+FOUNDATION_EXPORT const unsigned char BeaconConnectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeaconConnector/PublicHeader.h>
+
+
---
+++ BeaconConnector/BeaconConnector/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  BeaconConnector
+//
+//  Created by stone appy on 18/11/19.
+//  Copyright © 2019 stone appy. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){}
+    
+    public static func getLocation() -> String {
+        return "Beacon Device connected.."
+    }
+}
